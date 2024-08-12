56d9ad399c7e726ee53571050d2ba441f16368c1
rahulkumar-webdev <rahul.me.your@gmail.com>
2024-08-12 12:50:18 -0700

    changes in all

--- signup.js
+++ signup.js
@@ -1,3 +1,31 @@
-let inputs = document.querySelectorAll(input);
-console.log(inputs,"hay");
-console.log("hay")
+// let inputs = document.querySelectorAll("input");
+// let btn = document.querySelector("button");
+// let h1 = document.querySelector(".hh");
+
+
+// function res(){
+    
+//     let name = document.getElementById("name").value;
+//     document.write(`hello ${name} thanks you we will try respone as soon as possiable`)
+//     console.log(h1);
+    
+// }
+
+
+// btn.addEventListener("click",res);
+
+
+function emailsend() {
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "rahul.me.your@gmail.com",
+        Password : "00B8AE1DC2B412F14742C7C2C6D232BE1C8A",
+        To : 'rahul.me.your@gmail.com',
+        From : "rahul.me.your@gmail.com",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+
+}
