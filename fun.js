e5fa421608875a3455efb94c1e2fa131355b4067
rahulkumar-webdev <rahul.me.your@gmail.com>
2024-08-26 10:26:11 -0700

    change in midea query

---
+++ fun.js
@@ -0,0 +1,11 @@
+let baricon = document.querySelector(".baricon");
+let unorderlist = document.querySelector("ul");
+
+
+baricon.addEventListener("click",()=>{
+unorderlist.classList.toggle("showdata"); 
+
+
+
+ 
+})
